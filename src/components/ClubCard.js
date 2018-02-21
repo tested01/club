14bed44c4be960e27e6a5cf5ac593afd61010dee
testudn1 <fengpuyang@gmail.com>
2018-02-22 07:53:37 +0800

    mock data

--- src/components/ClubCard.js
+++ src/components/ClubCard.js
@@ -1,24 +1,90 @@
 import React, { Component } from 'react';
+  /*
+  //fetch example
+  fetch(CONFIG.API_BASE_URL.concat('/posts/unlike/').concat(this.props._id), {
+          method: 'PATCH',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'x-auth': this.props.loginState.xAuth //FIXME:guest has no xAuth
+              }
+         })
+          .then((response) => {
+            if (response.status === 200) {
 
+              response.json().then(json => {
+                                    //this.setState(Object.assign({}, this.state, json));
+                                    console.log('unlike', json)
+                                  });
+            } else {
+              console.log(response.status);
+            }
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+  */
+const urlPrefix = 'http://www.educoco.com/image?i=upload/';
 const styles = {
-  'thumbnail': {
+  'thumbnailContainer': {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
     width: '200px',
-    height: '200px'
+    height: '180px',
+    borderColor: 'black',
+    borderWidth: '2px',
+    borderRadius: '5px',
+    backgroundColor: '#F5F5F5',
+
+  },
+  'thumbnail': {
+    width: '80px',
+    height: '80px',
+    borderRadius: '50%'
+  },
+  'cardContainer': {
+    margin: '5px',
+    backgroundColor: '#F5F5F5',
+    borderRadius: '5px'
+  },
+  'infoContainer': {
+    margin: '5px',
+    backgroundColor: '#FFFFFF',
+    borderRadius: '5%'
+  },
+  'member': {
+    marginLeft: '5px',
+    marginRight: '5px',
+    color: 'gray',
+    fontSize: '10px'
+  },
+  'title': {
+    marginLeft: '5px',
+    marginRight: '5px',
+    fontSize: '12px'
   }
 };
 
 class ClubCard extends Component{
-
+  constructor(props){
+    super(props);
+  }
+  //'https://cdn2.ettoday.net/images/1756/d1756292.jpg'
   render(){
+    console.log(this.props);
     return(
-      <div>
-
-        <img
-          style={styles.thumbnail}
-          src='https://cdn2.ettoday.net/images/1756/d1756292.jpg'
-        />
-        <div> title </div>
-        <div> 1978 位社員 </div>
+      <div style={styles.cardContainer}>
+        <div style={styles.thumbnailContainer}>
+          <img
+            style={styles.thumbnail}
+            src={urlPrefix + this.props.thumbnail}
+          />
+        </div>
+        <div style={styles.infoContainer}>
+          <div style={styles.title}> {this.props.title} </div>
+          <div style={styles.member}> {this.props.member} 位社員 </div>
+        </div>
       </div>
     );
   }
--- src/components/app.js
+++ src/components/app.js
@@ -18,14 +18,22 @@ const styles = {
     justifyContent: 'flex-start'
   }
 };
-
+/*
+  Max title length: 14,
+  If the length is greater than 14 => abbreviation
+*/
 export default class App extends Component {
   render() {
     return (
       <div style={styles.cardContainer}>
-        { [1,2,3,4,5].map(
-          ()=>{
-            return(<ClubCard />);
+        { [
+          {'title': '社員社員社員社員社員社員社員', 'member': 123, 'thumbnail': 'group/a6a4c94f7c3a844ee8f9c99e14f404cfc0fb93d5.jpg'},
+          {'title': 'alf2', 'member': 123456, 'thumbnail': 'group/a5c25fd972df0ea6eb290e56067f9fd8488a00b9.jpg'},
+          {'title': 'alf3', 'member': 99, 'thumbnail': 'group/a6a4c94f7c3a844ee8f9c99e14f404cfc0fb93d5.jpg'},
+          {'title': 'alf4', 'member': 16, 'thumbnail': 'group/a5c25fd972df0ea6eb290e56067f9fd8488a00b9.jpg'}
+          ].map(
+          ( value, index )=>{
+            return(<ClubCard title={value.title} member={value.member} thumbnail={value.thumbnail}/>);
           }
         ) }
 
