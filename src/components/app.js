20d445e7ad34814dd332925dddcd576710d13bc3
testudn1 <fengpuyang@gmail.com>
2018-02-21 11:38:28 +0800

    flex layout

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 import ClubCard from './ClubCard';
 
+const styles = {
+  'cardContainer': {
+    /* We first create a flex layout context */
+    display: 'flex',
+
+    /* Then we define the flow direction
+       and if we allow the items to wrap
+     * Remember this is the same as:
+     * flex-direction: row;
+     * flex-wrap: wrap;
+     */
+    flexFlow: 'row wrap',
+
+    /* Then we define how is distributed the remaining space */
+    justifyContent: 'flex-start'
+  }
+};
+
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <div style={styles.cardContainer}>
         { [1,2,3,4,5].map(
           ()=>{
             return(<ClubCard />);
